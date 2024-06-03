f7e3c2552ac5b2457f1ef1bc314fb90c305650db
mumtozBEK1234 <email manzilingiz>
2024-06-03 17:41:47 +0300

    7 dars uy ishi

---
+++ index.js
@@ -0,0 +1,207 @@
+// 1 - masla switch 
+
+// switch (a = + prompt("ixtiyori sonni kiritin")) {
+//     case 1:
+//     case 2:
+//     case 3:
+//         console.log("Qish fasli");
+//         break;
+
+//     case 4:
+//     case 5:
+//     case 6:
+//         console.log("Bahor fasli");
+//         break;
+
+//     case 7:
+//     case 8:
+//     case 9:
+//         console.log("yoz fasli");
+//         break;
+
+//     case 10:
+//     case 11:
+//     case 12:
+//         console.log("kuz fasli");
+//         break;
+
+//     default:
+//         console.log("yilda 12 ta oy bor bilmesmi? murod aka");
+//         break;
+// }
+
+// // 2-masla switch
+
+// switch  (a = + prompt("ixtiyori sonni kiritin")) {
+//     case 100:
+//     case 99:
+//     case 98:
+//     case 97:
+//     case 96:
+//     case 95:
+//     case 94:
+//     case 93:
+//     case 92:
+//     case 91:
+//     case 90:
+//         console.log("A");
+
+//         break;
+
+//     case 90:
+//     case 89:
+//     case 88:
+//     case 87:
+//     case 86:
+//     case 85:
+//     case 84:
+//     case 83:
+//     case 82:
+//     case 81:
+//     case 80:
+//         console.log("B");
+
+//         break;
+
+
+//     case 80:
+//     case 79:
+//     case 78:
+//     case 77:
+//     case 76:
+//     case 75:
+//     case 74:
+//     case 73:
+//     case 72:
+//     case 71:
+//     case 70:
+//         console.log("C");
+
+//         break;
+
+
+//     case 70:
+//     case 69:
+//     case 68:
+//     case 67:
+//     case 66:
+//     case 65:
+//     case 64:
+//     case 63:
+//     case 62:
+//     case 61:
+//     case 60:
+//         console.log("D");
+
+//         break;
+
+//     case 60:
+//     case 0:
+//         console.log("F");
+
+//         break;
+
+//     default:
+//         console.log("bu bal mavjud emas");
+//         break;
+// }
+
+
+// 3-masla switch
+
+// switch (a = + prompt("ixtiyori vaqtni kiritin")) {
+//     case 5:
+//     case 6:
+//     case 7:
+//     case 8:
+//     case 9:
+//     case 10:
+//     case 11:
+//     case 12:
+//         console.log("xayirli tong");
+//         break;
+
+//     case 12:
+//     case 13:
+//     case 14:
+//     case 15:
+//     case 16:
+//     case 17:
+//         console.log("xayirli kun");
+//         break;
+
+//     case 17:
+//     case 18:
+//     case 19:
+//     case 20:
+//     case 21:
+ 
+//         console.log("xayirli kech");
+//         break;
+
+//     case 21:
+//     case 22:
+//     case 23:
+//     case 24:
+
+//         console.log("xayirli tun");
+//         break;
+
+//     default:
+//         console.log("bu vaqt yoq");
+//         break;
+// }
+
+
+// 4 - masala
+
+// let raqam = prompt('raqam yozing');
+
+// function togrison() {
+//     while (true) {
+//         if (raqam.toLowerCase() == 5) {
+//             alert(raqam);
+//             break;
+//         } else {
+//             raqam = prompt('raqam kiritin')
+//         }
+//     }
+// }
+
+
+// 5-masla 
+
+
+// function isim() {
+//     let s = 20;
+
+//     let i = 2;
+
+//     let sum = 0;
+
+//     while (i <= s) {
+
+//         console.log(i);
+
+//         i += 2;
+//     }
+
+// }
+
+
+// 6-masla
+
+// 6 - masala
+
+// let soz = prompt('ixtiyori sozni kiritin');
+
+// function togriSoz() {
+//     while (true) {
+//         if (soz.toLowerCase() == "stop") {
+//             alert(soz);
+//             break;
+//         } else {
+//             soz = prompt('ixtiyori sozni kiritin')
+//         }
+//     }
+// }
